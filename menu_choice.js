d7fe36447fb202f3001a6e0be0298062ec1c01f9
hrasek <hosekmotor@seznam.cz>
2023-08-26 18:28:13 +0200

    Update on wedding menu including first JavaScript attempts.

---
+++ menu_choice.js
@@ -0,0 +1,12 @@
+function Bezdiety() {
+  document.getElementById("bezdietyOutput").innerHTML =
+    "Hovězí vývar s domácími játrovými knedlíčky, nudlemi a zeleninou <br> Svíčková na smetaně s karlovarským knedlíkem, šlehačkou a brusinkami";
+}
+function Bezlepku() {
+  document.getElementById("bezlepkuOutput").innerHTML =
+    "Zeleninový krém <br> Svíčková na smetaně s bezlepkovým karlovarským knedlíkem, šlehačkou a brusinkami ";
+}
+function Vege() {
+  document.getElementById("vegeOutput").innerHTML =
+    "Zeleninový krém <br> Falafel s bramborem, zeleninová obloha ";
+}
